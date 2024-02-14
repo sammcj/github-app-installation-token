91d2623a904f6704c922473175661bbcc84a7075
Sam McLeod <sammcj@users.noreply.github.com>
2024-02-14 14:24:27 +1100

    improve tests and streamline code

--- index.js
+++ index.js
@@ -4,90 +4,86 @@ import jwt from 'jsonwebtoken';
 import * as core from '@actions/core';
 import { getInstallationId } from './lib/github-application.js';
 
-export function getInput() {
-  if (process.env.GITHUB_ACTIONS) {
-    const privateKey = core.getInput('application_private_key', { required: true });
-    const appId = core.getInput('application_id', { required: true });
-    const permissionsRaw = core.getInput('permissions');
-    // convert the string (e.g. "contents:read,actions:read" to an object, e.g. { contents: 'read', actions: 'read' })
-    const permissionsInput = permissionsRaw
-      ? permissionsRaw.split(',').reduce((acc, permission) => {
-          const [key, value] = permission.split(':');
-          acc[key] = value;
-          return acc;
-        }, {})
-      : {};
-
-    return { privateKey, appId, permissionsInput };
-  } else {
-    if (!process.env.GITHUB_APPLICATION_PRIVATE_KEY || !process.env.GITHUB_APPLICATION_ID) {
-      throw new Error('Required inputs are missing');
-    }
-    const privateKey = process.env.GITHUB_APPLICATION_PRIVATE_KEY.replace(/\\n/g, '\n');
-    const appId = process.env.GITHUB_APPLICATION_ID;
-    const permissionsRaw = process.env.GITHUB_PERMISSIONS ?? '{}';
-    const permissionsInput = permissionsRaw
-      ? permissionsRaw.split(',').reduce((acc, permission) => {
-          const [key, value] = permission.split(':');
-          acc[key] = value;
-          return acc;
-        }, {})
-      : {};
-    return { privateKey, appId, permissionsInput };
+/**
+ * Get the input from the environment or action inputs.
+ * @returns {Object} The private key, app ID, and permissions input.
+ */
+const getInput = () => {
+  const environment = process.env.GITHUB_ACTIONS ? 'action' : 'environment';
+  const requiredInputsMissing =
+    !process.env.GITHUB_APPLICATION_PRIVATE_KEY || !process.env.GITHUB_APPLICATION_ID;
+  if (environment === 'environment' && requiredInputsMissing) {
+    throw new Error('Required inputs are missing');
   }
-}
 
-export async function run() {
-  try {
-    // get the variables from the if/else block
-    const { privateKey, appId, permissionsInput } = getInput();
+  const privateKey = (
+    environment === 'action'
+      ? core.getInput('application_private_key', { required: true })
+      : process.env.GITHUB_APPLICATION_PRIVATE_KEY ?? ''
+  ).replace(/\\n/g, '\n');
+  const appId =
+    environment === 'action'
+      ? core.getInput('application_id', { required: true })
+      : process.env.GITHUB_APPLICATION_ID;
+  const permissionsRaw =
+    environment === 'action'
+      ? core.getInput('permissions')
+      : process.env.GITHUB_PERMISSIONS ?? '{}';
+  const permissionsInput = permissionsRaw.split(',').reduce((acc, permission) => {
+    const [key, value] = permission.split(':');
+    acc[key] = value;
+    return acc;
+  }, {});
 
-    if (!privateKey || !appId) {
-      throw new Error('Required inputs are missing: privateKey or appId is undefined');
-    }
+  return { privateKey, appId, permissionsInput };
+};
 
-    const permissions = permissionsInput;
-
-    // Ensure getInstallationId is correctly awaited and check its result before proceeding
-    const installationId = await getInstallationId(privateKey, appId);
-    if (!installationId) {
-      throw new Error('Failed to retrieve installation ID');
-    }
-    // console.debug('Retrieved Installation ID:', installationId);
-
-    const jwtToken = generateJwtToken(privateKey, appId);
-    const octokit = getOctokit(jwtToken);
-
-    const {
-      data: { token },
-    } = await octokit.rest.apps.createInstallationAccessToken({
-      installation_id: installationId,
-      permissions,
-    });
-
-    // output the permissions actually granted in the token
-    const installation = await octokit.rest.apps.getInstallation({
-      installation_id: installationId,
-    });
-
-    const permissionsGranted = installation.data.permissions;
-
-    core.setSecret(token);
-    core.setOutput('token', token);
-    core.setOutput('expires_at', new Date().toISOString());
-    core.setOutput('permissions_requested', JSON.stringify(permissions));
-    core.setOutput('permissions_granted', JSON.stringify(permissionsGranted));
-  } catch (error) {
-    console.error('Error:', error.message);
-    core.setFailed(error.message);
-  }
-}
-
-export function generateJwtToken(privateKey, appId) {
+/**
+ * Generate a JWT token.
+ * @param {string} privateKey - The private key.
+ * @param {string} appId - The app ID.
+ * @returns {string} The JWT token.
+ */
+const generateJwtToken = (privateKey, appId) => {
   const iat = Math.floor(Date.now() / 1000);
   const exp = iat + 10 * 60; // JWT expiration time set to 10 minutes
   const payload = { iat, exp, iss: appId };
   return jwt.sign(payload, privateKey, { algorithm: 'RS256' });
-}
+};
+
+/**
+ * The main function to run the action.
+ * @returns {Promise<void>}
+ */
+const run = async () => {
+  try {
+    const { privateKey, appId, permissionsInput } = getInput();
+    const installationId = await getInstallationId(privateKey, appId);
+    if (!installationId) throw new Error('Failed to retrieve installation ID');
+
+    const jwtToken = generateJwtToken(privateKey, appId);
+    const octokit = getOctokit(jwtToken);
+
+    const accessTokenResponse = await octokit.rest.apps.createInstallationAccessToken({
+      installation_id: installationId,
+      permissions: permissionsInput,
+    });
+
+    const permissionsGrantedResponse = await octokit.rest.apps.getInstallation({
+      installation_id: installationId,
+    });
+
+    core.setSecret(accessTokenResponse.data.token);
+    core.setOutput('token', accessTokenResponse.data.token);
+    core.setOutput('expires_at', accessTokenResponse.data.expires_at);
+    core.setOutput('permissions_requested', JSON.stringify(permissionsInput));
+    core.setOutput(
+      'permissions_granted',
+      JSON.stringify(permissionsGrantedResponse.data.permissions),
+    );
+  } catch (error) {
+    core.setFailed(`Error: ${error.message}`);
+  }
+};
 
 run();
